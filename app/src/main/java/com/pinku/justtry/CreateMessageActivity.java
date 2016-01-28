4e4de7e8fc08117d95e163ee1378ab5b512dcc0e
mobdev007 <pinkydey233@gmail.com>
2016-01-28 15:39:31 +0530

    onCreate method is created

--- app/src/main/java/com/pinku/justtry/CreateMessageActivity.java
+++ app/src/main/java/com/pinku/justtry/CreateMessageActivity.java
@@ -1,7 +1,21 @@
 package com.pinku.justtry;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 /**
  * Created by mine on 1/28/2016.
  */
-public class CreateMessageActivity {
+public class CreateMessageActivity extends Activity {
+    TextView em;
+    Button submit;
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createmessage_activity);
+
+        em = (TextView)findViewById(R.id.message);
+        submit=(Button) findViewById(R.id.sendmessage);
+    }
 }
