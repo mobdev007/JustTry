54dece267e4edc12301a7e947ddc39064d7f4a6e
mobdev007 <pinkydey233@gmail.com>
2016-01-28 17:58:28 +0530

    createmessage page is created

--- app/src/main/java/com/pinku/justtry/CreateMessageActivity.java
+++ app/src/main/java/com/pinku/justtry/CreateMessageActivity.java
@@ -1,21 +1,32 @@
 package com.pinku.justtry;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
  * Created by mine on 1/28/2016.
  */
-public class CreateMessageActivity extends Activity {
-    TextView em;
+public class CreateMessageActivity extends Activity implements View.OnClickListener {
+    EditText em;
     Button submit;
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createmessage_activity);
 
-        em = (TextView)findViewById(R.id.message);
+        em = (EditText)findViewById(R.id.editmessage);
         submit=(Button) findViewById(R.id.sendmessage);
+        submit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this,ReceiveMessageActivity.class);
+        startActivityForResult(i, 2);
     }
 }
